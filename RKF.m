b1cc344f3a099a1a2b4bc86022222b26aca5964a
SendMeYourCreditCardInfo <38713764+SendMeYourCreditCardInfo@users.noreply.github.com>
2018-04-29 16:43:01 -0700

    This is 100% done

--- RKF.m
+++ RKF.m
@@ -1,92 +1,63 @@
-function R = RKF(f,a,b,alpha,TOL,hmax,hmin)
-%Having a problem finding my exact h in this algortithm
-format long
-%   STEP 1
-    time=a; 
-    w=alpha; 
-    h = hmax;
-    FLAG=1;
-    N1 = 0;
-    N2 = 0;
-    N3 = 0;
-    T = zeros(1,N1+1); %an array of zeros
-    T(1,1) = time;
-    W =  zeros(1, N2+1);
-    W(1,1) = alpha;
-    H = zeros(1,N3+1);
-    H(1,1) = hmax;
+function Out = RKF3(f, a, b, alpha, TOL, hmax, hmin)
 
-    %STEP 2
-    while FLAG == 1
-        
-        %STEP 3
-        k1 = h*feval(f,T(1,N1+1),W(1,N2+1)); %feval = evalues function 
-        k2 = h*feval(f,T(1,N1+1)+(1/4)*h, w+(1/4)*k1);
-        k3 = h*feval(f,T(1,N1+1)+(3/8)*h, w+(3/32)*k1 + (9/32)*k2);
-        k4 = h*feval(f, T(1,N1+1)+(12/13)*h, w+(1932/2197)*k1 - (7200/2197)*k2+(7296/2197)*k3);
-        k5 = h*feval(f, T(1,N1+1)+h, w + (439/216)*k1-8*k2+(3680/513)*k3 - (845/4104)*k4);
-        k6 = h*feval(f, T(1,N1+1)+h/2, w - (8/27)*k1 + 2*k2 - (3544/2565)*k3 +(1859/4104)*k4 -(11/40)*k5);
-        %STEP 4 
-        %Note: R=(1/h)|w_tilde_(i+1) - w_(i+1)| ~= |t_(i+1)(h)|
-        R = (1/h)*abs((1/360)*k1 - (128/4275)*k3 - (2197/75240)*k4 + (1/50)*k5 + (2/55)*k6);
-        N1 = N1 + 1;
-        N2 = N2 + 1;
-     %  Debug
-     %  display(k1);
-     %  display(k2);
-     %  display(k3);
-     %  display(N1);
-     %  display(R);
-     %  display(T(1,N1));
-     %  fprintf(' k1 is %d\n  k2 is %d\n  k3 is %d\n  k4 is %d\n  k5 is %d\n  k6 is %d\n R is %d\n \n \n \n time is %d\n \n ',k1,k2,k3,k4,k5,k6,R,time);
-     %  fprintf('R < TOL ===> %d < %d\n \n \n',R,TOL);
-     % STEP 5
-        if R <= TOL
-            %STEP 6
-            %N1 = N1 +1;
-            T(1,N1+1) = T(1,N1) + h;
-            N2 = N2 +1;
-     %  display(N2);
-            w = W(1,N2-1) + (25/216)*k1 + (1408/2565)*k3 + (2197/4104)*k4 - (1/5)*k5;
-            W(1,N2+1) = w; 
-     %  display(W(1,N2+1));
-     %  display(T(1,N1+1));
-%  STEP 7
-          %  fprintf('START\nTime is %d, w is %d, h is %d\nEND\n \n \n \n \n',time,w,h);
-        end
-%  STEP 8
-        delta = 0.84*((TOL/R)^(1/4));
-%  STEP 9
-        if delta <= 0.1
-            h = 0.1*h;  
-        else if delta >= 4
-                h= 4*h;                              
-            else
-                h = 8*h;                          
-            end
-        end
-     %  STEP 10
-        if h > hmax
-            h = hmax;
-        end
-      % STEP 11
-        if T(1,N1) >= b
-            FLAG = 0;
-        else if T(1,N1) + h > b
-                h = b - T(1,N1);
-                                    
-            else if h < hmin
-                    FLAG = 0;
-                    fprintf('Minimum h has been exceeded; procedure completed unsuccessfully.');
-                end
-            end
-        end
-                                            N3 = N3 +1;
-                                    
-                                            H(1,N3+1) = h; %What is going on with the counter?
+t = a;
+w = alpha;
+h = hmax;
+flag =1;
+
+T = [];
+W = [];
+H = [];
+D = [];
+ri = [];
+c = 1;
+
+while (flag == 1)
+    k1 = h*f(t,w);
+    k2 = h*f(t +0.25*h, w +0.25*k1);
+    k3 = h*f(t+(3/8)*h, w +(3/32)*k1+(9/32)*k2);
+    k4 = h*f(t+(12/13)*h, w+(1932/2197)*k1-(7200/2197)*k2+(7296/2197)*k3);
+    k5 = h*f(t+h, w+(439/216)*k1 -8*k2+(3680/513)*k3-(845/4104)*k4);
+    k6 = h*f(t+ 0.5*h, w- (8/27)*k1 + 2*k2 - (3544/2565)*k3 + (1859/4104)*k4 - (11/40)*k5);
+    
+    R = (1/h)*abs((1/360)*k1 - (128/4275)*k3 - (2197/75240)*k4 + k5*(1/50) +(2/55)*k6);
+    
+    delta = .84*(TOL/R)^(1/4);
+    
+    if R <= TOL
+       t = t + h;
+       w = w + (25/216)*k1 + (1408/2565)*k3 + (2197/4104)*k4 - 0.2*k5;
+     %  fprintf('%d || %d || %d || %d || %d\n',t,w,h,delta,R);
+       T(c) = t; W(c) = w; H(c) = h; D(c) = delta; ri(c) = R;
+       
     end
-   % Debug and Final format 
+    
+    if delta < 0.1
+        h = 0.1 *h;
+    elseif delta >= 4
+        h = 4*h;
+    else
+        h = delta*h;
+    end
+    
+    if h > hmax
+        h = hmax;
+    end
+    
+    if t >= b
+        flag = 0;
+    elseif (t+h) > b
+        h = b -t;
+    elseif h < hmin
+        flag = 0;
+        fprintf('ERROR!');
+    end
+    c = c + 1;
+end
     ti = [T'];
-    %wi = [W'];
-    TABLE = table(ti);
-    R = TABLE
+    wi = [W'];
+    hi = [H'];
+    di = [D'];
+    Ri = [ri'];
+    Out = table(ti,wi,hi,di,Ri);
+end
