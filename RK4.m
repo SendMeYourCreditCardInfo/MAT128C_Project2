6fa00ee976f34cf3d228bccf9bb2790e0b9cc578
SendMeYourCreditCardInfo <38713764+SendMeYourCreditCardInfo@users.noreply.github.com>
2018-05-02 21:07:15 -0700

    Runge-Kutta 4th Oder

--- RK4.m
+++ RK4.m
@@ -1,17 +1,34 @@
-function R = RK4(f, a, b, h, alpha)
+function R = RK4(f, a, b,alpha, h)
     N = (b -a)/h;
     t = a;
     w = alpha;
     
+    Tm = zeros(1,1);
+    Tm(1,1) = a;
+    Tn = 1;
+    Wm = zeros(1,1);
+    Wm(1,1) = alpha;
+    Wn = 1;
+    
+    
     for i = 1:N
         k1 = h*feval(f, t,w);
-        Shit = t + (h/2);
-        Fuck = w + k1/2;
-        k2 = h*feval(f, Shit, Fuck);
+        k2 = h*feval(f, t + (h/2), w + k1/2);
         k3 = h*feval(f, t+(h/2), w+(k2/2));
         k4 = h*feval(f, t+h, w+k3);
         
         w = w + (1/6)*(k1 + 2*k2 + 2*k3 + k4);
         t = a + i*h;
-        fprintf('w%d is %d\n',i,w);
+        
+        Tn = Tn + 1;
+        Tm(1,Tn) = t;
+        Wn = Wn +1;
+        Wm(1,Wn) = w;
+        
+        
+        
+        %fprintf('w%d is %d\n',i,w);
     end
+    ti = [Tm'];
+    wi = [Wm'];
+    R = table(ti, wi);
